cafe8ecf03b4573ac91cd384c675759dda5bcda6
PK Banks <pkbanks12@gmail.com>
2018-02-13 17:50:49 -0500

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+cavs = Team.create(city: "Cleveland", name: "Cavs")
+
+lebron = Player.create(name: "Lebron James", position: "G", team_id: cavs.id)
+
+love = Player.create(name: "Kevin Love", position: "F", team_id: cavs.id)
+
+
+
+
+
+
+
